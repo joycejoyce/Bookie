465d53c5ab3641b1382812f86b09adc3bb5ebc94
Joyce <dorith1989@gmail.com>
2021-07-21 11:23:58 +0800

    Complete FollowUIDesignFlow

--- bookie/src/components/about/UIDesignFlow.js
+++ bookie/src/components/about/UIDesignFlow.js
@@ -1,22 +1,6 @@
 import UIDesignFlowCard from "./UIDesignFlowCard";
+import UserStory from "./UserStory.js";
 import LinkBtn from "./LinkBtn.js";
-
-function UserStory() {
-    const Pair = ({title, desc}) => (
-        <div className="pair">
-            <div className="title">{title}</div>
-            <div className="desc">{desc}</div>
-        </div>
-    );
-
-    return (
-        <div className="userStory">
-            <Pair title="As a" desc="book reader"/>
-            <Pair title="I want to" desc="make a to-read list"/>
-            <Pair title="To" desc="enrich my reading experience"/>
-        </div>
-    );
-}
 
 export default function UIDeisngFlow() {
     return (
@@ -35,6 +19,50 @@ export default function UIDeisngFlow() {
                     <LinkBtn
                         text="Functional Map"
                         link="https://drive.google.com/file/d/147xwMJWvZ3xwmdpNiVvM9lYsPgrLhMni/view?usp=sharing"
+                    />
+                }
+            />
+            <UIDesignFlowCard 
+                num="3"
+                title="Flow Chart"
+                desc="The flow of major functions"
+                backElem={
+                    <LinkBtn
+                        text="Flow Chart"
+                        link="https://docs.google.com/presentation/d/1_xY2oPmcajoFJkBx6FuaFJBbeHqTdn9KH93AXWWGk8Y/edit?usp=sharing"
+                    />
+                }
+            />
+            <UIDesignFlowCard 
+                num="4"
+                title="UI Flow"
+                desc="Use a diagram to explain which pages would be visited to achieve a certain task"
+                backElem={
+                    <LinkBtn
+                        text="UI Flow (sign-in)"
+                        link="https://drive.google.com/file/d/1QkRrWsty9u7rf5R5DLuOm1oJpuwMbGjw/view?usp=sharing"
+                    />
+                }
+            />
+            <UIDesignFlowCard 
+                num="5"
+                title="Wireframe"
+                desc="The blueprint of each page"
+                backElem={
+                    <LinkBtn
+                        text="Wireframe"
+                        link="https://www.figma.com/file/hlVjhmInLCDssjlMYgYpCN/Bookie-Wireframe"
+                    />
+                }
+            />
+            <UIDesignFlowCard 
+                num="6"
+                title="Mockup"
+                desc="The final look of each page"
+                backElem={
+                    <LinkBtn
+                        text="Mockup"
+                        link="https://www.figma.com/file/6vSZYTaM3GbYupyjTl5E6n/Bookie-Mockup"
                     />
                 }
             />
