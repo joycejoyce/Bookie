fa0bbb16fb985f3b9c4858247392b95f459e4c5e
Joyce <dorith1989@gmail.com>
2021-07-29 15:32:51 +0800

    Remove redundant files

--- bookie/src/components/icon/Logo.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import { ReactComponent as LogoImg } from '../../assets/logo.svg';
-
-const Logo = styled(LogoImg)`
-    width: 90px;
-`;
-
-export default Logo;
--- bookie/src/components/icon/Menu.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-import { ReactComponent as MenuImg } from '../assets/menu.svg';
-
-//height: 20px;
-const Menu = styled(MenuImg)`
-    width: 18px;
-`;
-
-export default Menu;
